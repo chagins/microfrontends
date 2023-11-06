dc8153c151b361b4c9f8975d0c8d0021ad8c6102
chagins <chagin_yav@hotmail.com>
2023-11-06 18:49:21 +0500

    feat(ecomm_cart): sharing module async bootstrap

--- ecomm/cart/src/index.js
+++ ecomm/cart/src/index.js
@@ -1,5 +1,1 @@
-import { faker } from '@faker-js/faker';
-
-const cartText = `<div>You have ${faker.number.int({ min: 0, max: 10 })} items in your cart</div>`;
-
-document.querySelector('#dev-cart').innerHTML = cartText;
+import('./bootstrap');
