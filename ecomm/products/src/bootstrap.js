100c56bdb5983232f048f092a9ff4d8506a5ce0a
chagins <chagin_yav@hotmail.com>
2023-11-19 21:20:34 +0500

    refactor(ecomm_products): add mount fn

--- ecomm/products/src/bootstrap.js
+++ ecomm/products/src/bootstrap.js
@@ -2,16 +2,26 @@ import { faker } from '@faker-js/faker';
 import moment from 'moment/moment';
 import { v4 } from 'uuid';
 
-const title = `<h1>Products container ${v4()}</h1>`
-const date = `<p>${moment()}</p>`
-let products = '';
+export const mount = (rootEl) => {
+  const title = `<h1>Products container ${v4()}</h1>`
+  const date = `<p>${moment()}</p>`
+  let products = '';
 
-for (let i = 0; i < 5; i++) {
-  const name = faker.commerce.productName();
-  products += `<div>${name}</div>`;
+  for (let i = 0; i < 5; i++) {
+    const name = faker.commerce.productName();
+    products += `<div>${name}</div>`;
+  }
+
+  const productsContainer = `<div>${title}${date}${products}</div>`;
+
+  rootEl.innerHTML = productsContainer;
 }
 
-const productsContainer = `<div>${title}${date}${products}</div>`;
+if (process.env.NODE_ENV === 'development') {
+  const rootEl = document.querySelector('#dev-products');
 
-document.querySelector('#dev-products').innerHTML = productsContainer;
-
+  // running in isolation
+  if (rootEl) {
+    mount(rootEl);
+  }
+}
