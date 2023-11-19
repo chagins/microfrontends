5658c88c1f09696a78d14a9fbc20f654fbfdc1ee
chagins <chagin_yav@hotmail.com>
2023-11-19 14:19:53 +0500

    feat(ecomm_products): add moment@2.29.4 & date block

--- ecomm/products/src/bootstrap.js
+++ ecomm/products/src/bootstrap.js
@@ -1,5 +1,8 @@
 import { faker } from '@faker-js/faker';
+import moment from 'moment/moment';
 
+const title = `<h1>Products container</h1>`
+const date = `<p>${moment()}</p>`
 let products = '';
 
 for (let i = 0; i < 5; i++) {
@@ -7,4 +10,7 @@ for (let i = 0; i < 5; i++) {
   products += `<div>${name}</div>`;
 }
 
-document.querySelector('#dev-products').innerHTML = products;
+const productsContainer = `<div>${title}${date}${products}</div>`;
+
+document.querySelector('#dev-products').innerHTML = productsContainer;
+
